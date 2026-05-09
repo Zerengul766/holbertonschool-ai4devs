be9c88371a81b461e9b4280354c86a0cc57cc7ad
Zerengul <zerengulserifzade796@gmail.com>
2026-05-09 23:57:55 +0400

    Update bug4.js

--- prompting_debug_assistant/bug_snippets/bug4.js
+++ prompting_debug_assistant/bug_snippets/bug4.js
@@ -1,10 +1,25 @@
-async function getUserNames(url) {
+async function getProductTitles(apiUrl) {
+    const response = fetch(apiUrl);
 
-    const response = fetch(url);        
-    const data = response.json();       
-    const names = data.map(user => user.name.toUpperCase());
-    return names;
+    const data = response.json();
+
+    const titles = data.map(item => item.title.toLowerCase());
+
+    return titles;
 }
 
-const result = getUserNames("https://jsonplaceholder.typicode.com/users");
-console.log("Names:", result);
+
+async function printReport() {
+    const result = getProductTitles("https://fakestoreapi.com/products");
+
+    console.log("Product titles:");
+
+    for (let i = 0; i < result.length; i++) {
+        console.log(result[i]);
+    }
+
+    console.log("Done");
+}
+
+
+printReport();
