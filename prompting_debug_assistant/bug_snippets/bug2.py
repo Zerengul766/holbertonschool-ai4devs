e22b62640b5c6c5d8b37869fe1368dfa19d1e478
Zerengul <zerengulserifzade796@gmail.com>
2026-05-09 23:55:47 +0400

    Update bug2.py

--- prompting_debug_assistant/bug_snippets/bug2.py
+++ prompting_debug_assistant/bug_snippets/bug2.py
@@ -1,12 +1,47 @@
-def factorial(n):
-    if n == 1:
-        return 1
-    result = 0
-    for i in range(1, n):
-        result *= i
-    return result
+def calculate_total_price(prices, discount_percent):
+    total = 0
+
+    for price in prices:
+        total += price
+
+    discount_amount = total * discount_percent
+    final_price = total - discount_amount
+
+    return final_price
 
 
-print(factorial(5))
-print(factorial(1))
-print(factorial(0))
+def print_receipt(items, prices):
+    print("----- RECEIPT -----")
+
+    for i in range(len(items)):
+        print(items[i], "-", prices[i], "AZN")
+
+    print("-------------------")
+
+
+shopping_items = [
+    "Keyboard",
+    "Mouse",
+    "Monitor",
+    "USB Cable",
+    "Headphones"
+]
+
+shopping_prices = [
+    45,
+    25,
+    300,
+    10,
+    80
+]
+
+discount = 20
+
+print_receipt(shopping_items, shopping_prices)
+
+result = calculate_total_price(
+    shopping_prices,
+    discount
+)
+
+print("Final price:", result, "AZN")
