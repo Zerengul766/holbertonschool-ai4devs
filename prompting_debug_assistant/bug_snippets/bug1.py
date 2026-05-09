b25bda40252e76abb4ae115952de3c3072ef1461
Zerengul <zerengulserifzade796@gmail.com>
2026-05-09 23:54:23 +0400

    Refactor code to calculate and print average scores

--- prompting_debug_assistant/bug_snippets/bug1.py
+++ prompting_debug_assistant/bug_snippets/bug1.py
@@ -1,11 +1,39 @@
-def get_last_n(items, n):
-    result = []
-    start = len(items) - n
-    for i in range(start, len(items) + 1):
-        result.append(items[i])
-    return result
+def calculate_average_scores(scores):
+    total = 0
+
+    for i in range(len(scores) + 1):
+        total += scores[i]
+
+    average = total / len(scores)
+
+    return average
 
 
-my_list = [10, 20, 30, 40, 50]
-print(get_last_n(my_list, 3))
-print(get_last_n(my_list, 5))
+def print_student_report(names, scores):
+    for i in range(len(names)):
+        print("Student:", names[i])
+        print("Score:", scores[i])
+        print("-------------------")
+
+
+student_names = [
+    "Ali",
+    "Leyla",
+    "Murad",
+    "Nigar",
+    "Aysel"
+]
+
+student_scores = [
+    78,
+    85,
+    92,
+    88,
+    95
+]
+
+print_student_report(student_names, student_scores)
+
+avg = calculate_average_scores(student_scores)
+
+print("Average score:", avg)
